ea48c1afa49358fa6521e71340b351c5e0bc230a
IOKG04 <iokg04@gmail.com>
2024-03-01 21:32:59 +0100

    Better enemy ai
    it cant see u through walls, and can lose track of you if youre too far away
    
    i should probably rename the file from "WalkGuy.cs" to "Enemy.cs" or something
    or i could not do that and just leave it in for people forking this to wonder why its called that, then read this commit description and wonder if i ever actually thought about someone forking this or if im just saying this to annoy them

--- Assets/Scripts/TAServer.cs
+++ Assets/Scripts/TAServer.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class TAServer : MonoBehaviour{
     public float timeScale, distanceScale;
+
+    public float LocalTimeScale(Vector2 position){
+        return 1 + Mathf.Pow(((Vector2)transform.position - position).magnitude * distanceScale, 0.5f) * timeScale;
+    }
 }
--- Assets/Scripts/WalkGuy.cs
+++ Assets/Scripts/WalkGuy.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class WalkGuy : MonoBehaviour{
@@ -12,36 +14,91 @@ public class WalkGuy : MonoBehaviour{
     private bool accelerating; // used as switch between accelerating and decelerating, just dont take this name too literally, look at the code before u change it
     public float accelerationTimer, accelerationTime;
 
+    public CircleCollider2D trigger;
+    public float viewCheckTimer, viewCheckTime;
+
+    public bool active;
+
     public TAServer taServer;
     private float localTimeScale;
 
     void Start(){
         rb = gameObject.GetComponent<Rigidbody2D>();
+        distanceMaxDifference *= Random.Range(0.9f, 1.1f);
+        accelerationTime *= Random.Range(0.95f, 1.05f);
+        speed *= Random.Range(0.9f, 1.1f);
+        viewCheckTimer = Random.Range(-0.2f, 0f);
+        viewCheckTime *= Random.Range(0.95f, 1.05f);
+        trigger = gameObject.GetComponent<CircleCollider2D>();
+        trigger.radius *= Random.Range(0.9f, 1.1f);
     }
     void FixedUpdate(){
         // calculate local time scale
-        localTimeScale = 1 + Mathf.Pow((taServer.transform.position - transform.position).magnitude * taServer.distanceScale, 0.5f) * taServer.timeScale;
+        localTimeScale = taServer.LocalTimeScale(transform.position);
 
-        // calculate movement
-        Vector2 targetPosition = (rb.position - target.position).normalized * distanceToTarget + target.position;
-        Vector2 targetVelocity;
-        if((rb.position - targetPosition).magnitude > distanceMaxDifference){
-            targetVelocity = (targetPosition - (Vector2)transform.position).normalized * speed;
-            if(!accelerating){
-                accelerating = true;
-                accelerationTimer = 0f;
-                lastVelocity = rb.velocity;
+        if(active){
+            // calculate movement
+            Vector2 targetPosition = (rb.position - target.position).normalized * distanceToTarget + target.position;
+            Vector2 targetVelocity;
+            if((rb.position - targetPosition).magnitude > distanceMaxDifference){
+                targetVelocity = (targetPosition - (Vector2)transform.position).normalized * speed;
+                if(!accelerating){
+                    accelerating = true;
+                    accelerationTimer = 0f;
+                    lastVelocity = rb.velocity;
+                }
+            }
+            else{
+                targetVelocity = Vector2.zero;
+                if(accelerating){
+                    accelerating = false;
+                    accelerationTimer = 0f;
+                    lastVelocity = rb.velocity;
+                }
+            }
+            accelerationTimer += Time.fixedDeltaTime * localTimeScale;
+            rb.velocity = Vector2.Lerp(lastVelocity, targetVelocity, Mathf.Sqrt(accelerationTimer / accelerationTime)) * localTimeScale;
+
+            // check for player visibility
+            viewCheckTimer += Time.fixedDeltaTime * localTimeScale;
+            if(viewCheckTimer > viewCheckTime){
+                viewCheckTimer = 0f;
+                bool hitPlayer = false;
+                RaycastHit2D[] hits = Physics2D.RaycastAll(rb.position, target.position - rb.position, trigger.radius);
+                for(int i = 0; i < hits.Length; i++){
+                    if(hits[i].collider.gameObject.CompareTag("Wall")) break;
+                    if(hits[i].collider.gameObject.CompareTag("Player")){
+                        hitPlayer = true;
+                        break;
+                    }
+                }
+                if(!hitPlayer){
+                    active = false;
+                    accelerationTimer = 0f;
+                    lastVelocity = rb.velocity;
+                }
             }
         }
         else{
-            targetVelocity = Vector2.zero;
-            if(accelerating){
-                accelerating = false;
-                accelerationTimer = 0f;
-                lastVelocity = rb.velocity;
+            accelerationTimer += Time.fixedDeltaTime * localTimeScale;
+            rb.velocity = Vector2.Lerp(lastVelocity, Vector2.zero, Mathf.Sqrt(accelerationTimer / accelerationTime)) * localTimeScale;
+        }
+    }
+    void OnTriggerStay2D(Collider2D col){
+        if(!active && col.gameObject.CompareTag("Player")){
+            viewCheckTimer += Time.fixedDeltaTime * localTimeScale;
+            if(viewCheckTimer > viewCheckTime){
+                viewCheckTimer = 0f;
+                RaycastHit2D[] hits = Physics2D.RaycastAll(rb.position, (Vector2)col.gameObject.transform.position - rb.position, trigger.radius);
+                for(int i = 0; i < hits.Length; i++){
+                    if(hits[i].collider.gameObject.CompareTag("Wall")) break;
+                    if(hits[i].collider.gameObject.CompareTag("Player")){
+                        active = true;
+                        target = hits[i].collider.gameObject.GetComponent<Rigidbody2D>();
+                        break;
+                    }
+                }
             }
         }
-        accelerationTimer += Time.fixedDeltaTime * localTimeScale;
-        rb.velocity = Vector2.Lerp(lastVelocity, targetVelocity, Mathf.Sqrt(accelerationTimer / accelerationTime)) * localTimeScale;
     }
 }
