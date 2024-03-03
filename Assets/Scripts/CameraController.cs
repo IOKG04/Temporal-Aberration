243ebeb860d3628857f91bcfed68e0293573eef1
IOKG04 <iokg04@gmail.com>
2024-03-03 17:32:11 +0100

    May I present: The first demo level thats actually a level
    Now featuring a bunch of empty rooms and enough enemies to kill you if youre bad
    
    (also a couple smaller changes if u care for that)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour{
     public Transform target;
     public float zOffset;
 
+    public Camera thisCamera;
+
     void Start(){
+        thisCamera = gameObject.GetComponent<Camera>();
+        //thisCamera.aspect = 16f / 9f;
     }
     void Update(){
         Vector3 finalTarget = target.position + new Vector3(0, 0, zOffset);
